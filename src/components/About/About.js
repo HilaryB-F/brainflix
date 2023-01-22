bfd132f2c35c121d613dc8c1063049f0b37f525c
Hilary <hilarybrittonfoster@gmail.com>
2023-01-22 12:39:34 -0800

    BEM updated, and divs switched when necessary

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,35 +4,35 @@ import ViewIcon from "../../assets/images/icons/Icons/views.svg";
 
 export default function About({currentVid}) {
   return (
-    <div>
+    <>
       <h1 className="about__title">{currentVid.title}</h1>
-      <div className="about__container">
-        <div className="about__info">
+      <main className="about__container">
+        <section className="about__info">
           <p className="about__info-title">By {currentVid.channel}</p>
           <p className="about__info-timestamp">
             {new Date(currentVid.timestamp).toLocaleDateString()}
           </p>
-        </div>
-        <div className="about__icons">
-          <div className="about__icons--icon">
+        </section>
+        <section className="about__icons">
+          <div className="about__icons-icon">
             <img
-              className="about__icons--icon--img"
+              className="about__icons-icon-img"
               src={ViewIcon}
               alt="Eye to represent it has been seen"
             ></img>
             <p>{currentVid.views}</p>
           </div>
-          <div className="about__icons--icon about__icon--icon-like">
+          <section className="about__icons-icon about__icon-icon-like">
             <img
-              className="about__icons--icon--img"
+              className="about__icons-icon-img"
               src={LikeIcon}
               alt="Heart"
             ></img>
             <p>{currentVid.likes}</p>
-          </div>
-        </div>
-      </div>
+          </section>
+        </section>
+      </main>
       <p className="about__info-description">{currentVid.description}</p>
-    </div>
+    </>
   );
 }
